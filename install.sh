2964fe0c9dfc3dad55a27aced7ecb79aab756f0a
Stephen Sherratt <steve@stevebob.net>
2011-07-28 22:48:54 +1000

    verbose install

--- install.sh
+++ install.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-v=$(./version.py | head -n1 | cut -d'.' -f1)
+echo Checking for python...
 
 p=$(which python);
 
 
 if [ $p ]; then
+    echo Python installation found. Checking version...
+    v=$(./version.py | head -n1 | cut -d'.' -f1)
     
     mkdir tmp
+
+    echo Prepending server with python location...
     echo \#!$p > tmp/simpleserver
     
+    echo Copying code...
     cat components/simpleserver.py >> tmp/simpleserver
     
     if [ $v = "2" ]; then
@@ -21,14 +26,27 @@ if [ $p ]; then
         2to3 tmp/simpleserver
     
     fi;
-
+    
+    echo Copying server wrapper...
     cp components/startmdserver tmp
-
+    
+    echo Setting permissions...
     chmod +x tmp/*
 
+    echo Copying files...
     mv tmp/* /usr/bin/
+
+    echo Cleaning up...
     rm -r tmp
 
+    echo Installation complete!
+    echo
+    echo Usage Instructions
+    echo 1. Navigate to a directory containing markdown files.
+    echo 2. Run the command \"startmdserver\".
+    echo 3. In your web browser, visit localhost:8080.
+    echo
+    
 else
 
     echo "Error: Python is not installed. Please install python.";
